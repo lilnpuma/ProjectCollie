bcbfd96608c997f5786a10b0d70ae763dc17ad68
lilnpuma <manump@terpmail.umd.edu>
2022-12-11 03:13:52 -0500

    3.008 Created a node file for motion stack

---
+++ panda_controller/include/panda_controller/motion_node.cpp
@@ -0,0 +1,44 @@
+/**
+ * @file motion_node.cpp
+ * @author Akash Ravindra (aravind2@umd.edu)
+ * @brief
+ * @version 0.1
+ * @date 2022-12-13
+ *
+ * MIT License
+ * Copyright (c) 2022 Akash Ravindra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+
+#include <rclcpp/rclcpp.hpp>
+
+#include "motion_stack.hpp"
+
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+  auto motion_stack =
+      std::make_shared<pc_moveit::MotionStack>("panda_controller_motion");
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(motion_stack);
+  executor.spin();
+  rclcpp::shutdown();
+  return 0;
+}
