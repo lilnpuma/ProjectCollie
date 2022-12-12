461e6d188072929839dfa20cea782b382d6e4d61
lilnpuma <manump@terpmail.umd.edu>
2022-12-11 19:10:26 -0500

    3.017, 3.018 Lint all cpp and hpp files for MotionStack and ImgStack.

--- panda_controller/src/img_stack.cpp
+++ panda_controller/src/img_stack.cpp
@@ -12,9 +12,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,11 +23,12 @@
  */
 
 #include "img_stack.hpp"
+
 #include <cmath>
+
 #include "geometry_msgs/msg/pose2_d.hpp"
 
 vis::ImgStack::ImgStack(std::string name) : Node(name) {
-
   subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
       "camera/image_raw", 10,
       std::bind(&ImgStack::image_callback, this, std::placeholders::_1));
@@ -38,7 +39,6 @@ vis::ImgStack::ImgStack(std::string name) : Node(name) {
 vis::ImgStack::~ImgStack() { cv::destroyWindow("view"); }
 
 void vis::ImgStack::image_callback(const sensor_msgs::msg::Image &msg) {
-
   vis::ImgStack::input_image = cv_bridge::toCvCopy(msg, "bgr8")->image;
   cv::imshow("view", vis::ImgStack::input_image);
   cv::waitKey(3);
@@ -47,7 +47,6 @@ void vis::ImgStack::image_callback(const sensor_msgs::msg::Image &msg) {
 void vis::ImgStack::get_img() {}
 
 cv::Mat vis::ImgStack::thresh_img(cv::Mat image) {
-
   cv::Mat color_img;
   cv::threshold(image, color_img, 180, 255, cv::THRESH_BINARY);
   cv::imshow("view", color_img);
@@ -56,7 +55,6 @@ cv::Mat vis::ImgStack::thresh_img(cv::Mat image) {
 }
 
 cv::Mat vis::ImgStack::find_contour(cv::Mat image) {
-
   cv::Mat edges, blur_img;
   /// Blur image
   cv::GaussianBlur(image, blur_img, cv::Size(3, 3), 0);
@@ -68,7 +66,6 @@ cv::Mat vis::ImgStack::find_contour(cv::Mat image) {
 }
 
 void vis::ImgStack::get_coords(cv::Mat image, std::string color) {
-
   std::vector<std::vector<cv::Point>> contours;
   std::vector<cv::Vec4i> hierarchy;
   /// Get Contours
@@ -85,38 +82,35 @@ void vis::ImgStack::get_coords(cv::Mat image, std::string color) {
   ///  Get the centroid of figures and orientation
   std::vector<std::vector<cv::Point2f>> rect(contours.size());
   std::vector<panda_world::msg::BlockLocation> block_loc;
-  
 
-   for (int i = 0; i < contours_size; i++) {
+  for (int i = 0; i < contours_size; i++) {
     cv::boxPoints(cv::minAreaRect(contours[i]), rect[i]);
     geometry_msgs::msg::Pose2D new_pose;
     new_pose.x = (mu[i].m10 / mu[i].m00 - image.size[0] / 2) * scaling_factor;
     new_pose.y = (mu[i].m01 / mu[i].m00 - image.size[1] / 2) * scaling_factor;
-    
 
     if (rect[i][1].x == rect[i][0].x) {
-        new_pose.theta = 0.0;
-    }
-     else {
-        new_pose.theta = atan2((rect[i][1].y - rect[i][0].y), (rect[i][1].x - rect[i][0].x));
-        
+      new_pose.theta = 0.0;
+    } else {
+      new_pose.theta =
+          atan2((rect[i][1].y - rect[i][0].y), (rect[i][1].x - rect[i][0].x));
     }
     panda_world::msg::BlockLocation new_loc;
     new_loc.color = color;
     new_loc.pose = new_pose;
-    
+
     vis::ImgStack::block_locations.blocks.push_back(new_loc);
   }
 }
 
 void vis::ImgStack::block_locator(cv::Mat image) {
   cv::Mat color_img[3], thres_img;
-  cv::split (image, color_img);
+  cv::split(image, color_img);
   std::vector<std::string> colors;
   colors.push_back("blue");
   colors.push_back("green");
   colors.push_back("red");
-  std::vector<panda_world::msg::MultiBlockLocation> block_loc (3);
+  std::vector<panda_world::msg::MultiBlockLocation> block_loc(3);
   for (int i = 0; i < 3; i++) {
     color_img[i] = vis::ImgStack::thresh_img(color_img[i]);
     color_img[i] = vis::ImgStack::find_contour(color_img[i]);
--- panda_controller/src/motion_stack.cpp
+++ panda_controller/src/motion_stack.cpp
@@ -1,24 +1,24 @@
 /**
  * @file motion_stack.cpp
  * @author Akash Ravindra (aravind2@umd.edu)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-12-14
- * 
+ *
  * Copyright [2022] [Akash Ravindra, Manu Madhu Pillai]
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *  http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * 
+ *
  */
 #include "motion_stack.hpp"
 
@@ -28,21 +28,21 @@ MotionStack::MotionStack(std::string name)
       m_arm_group_(std::shared_ptr<rclcpp::Node>(std::move(this)), ARM_GROUP),
       m_gripper_group_(std::shared_ptr<rclcpp::Node>(std::move(this)),
                        HAND_GROUP) {
-    // Subscribe to the block location topic
+  // Subscribe to the block location topic
   m_block_locations_sub_ =
       this->create_subscription<panda_world::msg::MultiBlockLocation>(
           "/block_info", 10,
           std::bind(&MotionStack::blockLocationCallback, this,
                     std::placeholders::_1));
-    m_timer_ = this->create_wall_timer(
-      std::chrono::milliseconds(1000),
-      std::bind(&MotionStack::performPickPlace, this));
-      m_timer_->reset();
+  m_timer_ =
+      this->create_wall_timer(std::chrono::milliseconds(1000),
+                              std::bind(&MotionStack::performPickPlace, this));
+  m_timer_->reset();
 }
 MotionStack::~MotionStack() {}
 void MotionStack::blockLocationCallback(
     const panda_world::msg::MultiBlockLocation& msg) {
-    m_timer_->cancel();
+  m_timer_->cancel();
   RCLCPP_INFO(this->get_logger(), "Received block info");
   // Store all the block locations
   for (auto block : msg.blocks) {
@@ -59,7 +59,8 @@ void MotionStack::blockLocationCallback(
 
 geometry_msgs::msg::Pose MotionStack::convertPose2DToPose(
     geometry_msgs::msg::Pose2D pose2d, float z = 0.0) {
-      // This function is used to convert a 2d pose to a 3d pose with a given z offset
+  // This function is used to convert a 2d pose to a 3d pose with a given z
+  // offset
   geometry_msgs::msg::Pose pose;
   pose.position.x = pose2d.x;
   pose.position.y = pose2d.y;
@@ -99,7 +100,7 @@ geometry_msgs::msg::Pose MotionStack::getPlacePose(std::string& block_name) {
 }
 geometry_msgs::msg::Pose MotionStack::getPickPose(
     geometry_msgs::msg::Pose2D block_pose, bool pre_grasp = false) {
-      // Pick pose is either above the block or at the block
+  // Pick pose is either above the block or at the block
   return convertPose2DToPose(block_pose,
                              pre_grasp ? PRE_GRASP_HEIGHT : PICK_HEIGHT);
 }
@@ -124,7 +125,7 @@ void MotionStack::moveGripper(bool open = true) {
   m_gripper_group_.move();
 }
 void MotionStack::performPickPlace() {
-  while(!m_block_locations_.empty()) {
+  while (!m_block_locations_.empty()) {
     // Stop the timer
     m_timer_->cancel();
     auto block = m_block_locations_.front();
@@ -136,7 +137,8 @@ void MotionStack::performPickPlace() {
     moveGripper(true);
     moveToPose(getPickPose(block.second));
     moveGripper(false);
-    RCLCPP_INFO(this->get_logger(), "Finished Picking block %s", block.first.c_str());
+    RCLCPP_INFO(this->get_logger(), "Finished Picking block %s",
+                block.first.c_str());
     rclcpp::sleep_for(std::chrono::milliseconds(100));
     // Place
     moveToPose("ready");
